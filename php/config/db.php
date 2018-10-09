d391a7f6035753ba56bb171c80f8f012a1a7dffc
Mikhail Kuklin <mk@turbodealer.ru>
2018-10-09 18:28:14 +0300

    db

--- php/config/db.php
+++ php/config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=alarm',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => getenv('APP_ROLE') == 'production' ? 'mysql:host=localhost;dbname=alarmdb' : 'mysql:host=localhost;dbname=alarm',
+    'username' => getenv('APP_ROLE') == 'production' ? 'alarmuser' : 'root',
+    'password' => getenv('APP_ROLE') == 'production' ? 'CW8OFDmz1yp6' : '',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
