d03ef8addcc9b664de28f7aa8e996bb2e898f69b
Mikhail Kuklin <mk@turbodealer.ru>
2018-10-09 18:36:57 +0300

    db creds for nodejs

--- js/creds.js
+++ js/creds.js
@@ -2,7 +2,7 @@ module.exports = {
     username: 'KRU7802400',
     password: 'KIAoptima@78024',
 
-    mysqlHost: '127.0.0.1',
+    mysqlHost: 'localhost',
     mysqlUsername: 'alarmuser',
     mysqlPassword: 'CW8OFDmz1yp6',
     mysqlDatabase: 'alarmdb'
