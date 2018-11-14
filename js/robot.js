489aa8fece3d80cbe504e6ecc03867fc21b3e1b0
Mikhail Kuklin <mk@turbodealer.ru>
2018-11-14 12:05:22 +0300

    change delay to 59 seconds

--- js/robot.js
+++ js/robot.js
@@ -38,7 +38,7 @@ function delay(ms) {
 }
 
 async function run() {
-    await delay(54000);
+    await delay(59000);
     const connection = await connectToDb();
     await robot(connection);
     disconnectFromDb(connection);
