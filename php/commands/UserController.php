394d727f4584a541284776487ec25f2f2556b865
Mikhail Kuklin <mk@turbodealer.ru>
2019-02-12 11:50:32 +0300

    Добавил команду для сброса пароля пользователя

--- php/commands/UserController.php
+++ php/commands/UserController.php
@@ -3,6 +3,7 @@
 namespace app\commands;
 
 use app\models\SignupForm;
+use app\models\User;
 use app\models\Task;
 use Yii;
 use yii\console\Controller;
@@ -24,6 +25,25 @@ class UserController extends Controller
         } else {
             echo "User not created: " . var_export($signupForm->errors, true).PHP_EOL;
         }
+    }
+
+    /**
+     * Setup new password for user with given email
+     *
+     * @param $email
+     * @throws \yii\base\Exception
+     */
+    public function actionResetPassword($email)
+    {
+        $user = User::findOne(['email' => $email]);
+        if (!$user) {
+            echo "User with email {$email} not found\n";
+            die;
+        }
+        $newPassword = Yii::$app->security->generateRandomString();
+        $user->setPassword($newPassword);
+        $user->save(false);
+        echo "Password for user {$email} changed to {$newPassword}\n";
     }
 
     public function actionAboutSimilarTask()
