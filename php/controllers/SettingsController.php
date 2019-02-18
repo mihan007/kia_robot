737ef1575611063794a175e0c7f643e4ede0d852
Mikhail Kuklin <mk@turbodealer.ru>
2019-02-18 13:16:17 +0300

    Корректно уведомляем пользователя на странице настроек о том, что задачи запущены/не запущены штатно после блокировки. И если не запущены, то объясняем, что надо сделать
    @see https://trello.com/c/oEH3ZLUL/1530-написать-уведомление-если-робот-не-может-туда-зайти-по-аларму-моторленду-и-будущим-салонам-кто-работает-по-данному-продукту

--- php/controllers/SettingsController.php
+++ php/controllers/SettingsController.php
@@ -29,15 +29,25 @@ class SettingsController extends Controller
                     throw new HttpException(404, 'Компания не найдена');
                 }
                 if (Yii::$app->request->isPost) {
+                    $oldPassword = $company->kia_password;
                     $result = $company->setNotificationEmail(Yii::$app->request->post('email'));
                     $company->kia_login = Yii::$app->request->post('kia_login');
                     $company->kia_password = Yii::$app->request->post('kia_password');
-                    $company->banned_at = null;
+                    $newPassword = $company->kia_password;
+                    $successMessage = 'Настройки успешно сохранены';
+                    if ($company->banned_at > 0) {
+                        $company->banned_at = null;
+                        if ($newPassword != $oldPassword) {
+                            $successMessage .= '. Так как вы изменили пароль доступа к сайту Киа, то признак закрытия доступа снят и все задачи вскоре начнут работать штатно.';
+                        } else {
+                            Yii::$app->session->addFlash('warning', "Вам необходимо внести корректный пароль от сайта Киа для дилера {$company->name}, т.к. на текущий момент задачи не выполняются.");
+                        }
+                    }
                     if ($result === true) {
                         $company->save(false);
-                        Yii::$app->getSession()->setFlash('success', 'Настройки успешно сохранены');
+                        Yii::$app->getSession()->setFlash('success', $successMessage);
                     } else {
-                        Yii::$app->getSession()->setFlash('error', 'Ошибка сохранения email для уведомления. Следующие email некорректны: '.implode(', ', $result));
+                        Yii::$app->getSession()->setFlash('error', 'Ошибка сохранения email для уведомления. Следующие email некорректны: ' . implode(', ', $result));
                     }
                     return $this->redirect(['settings/index', 'company_id' => $company_id]);
                 }
@@ -52,13 +62,23 @@ class SettingsController extends Controller
                 throw new HttpException(404, 'Компания не найдена');
             }
             if (Yii::$app->request->isPost) {
+                $oldPassword = $company->kia_password;
                 $result = $company->setNotificationEmail(Yii::$app->request->post('email'));
                 $company->kia_login = Yii::$app->request->post('kia_login');
                 $company->kia_password = Yii::$app->request->post('kia_password');
-                $company->banned_at = null;
+                $newPassword = $company->kia_password;
+                $successMessage = 'Настройки успешно сохранены';
+                if ($company->banned_at > 0) {
+                    $company->banned_at = null;
+                    if ($newPassword != $oldPassword) {
+                        $successMessage .= '. Так как вы изменили пароль доступа к сайту Киа, то признак закрытия доступа снят и все задачи вскоре начнут работать штатно.';
+                    } else {
+                        Yii::$app->session->addFlash('warning', "Вам необходимо внести корректный пароль от сайта Киа, т.к. на текущий момент задачи не выполняются.");
+                    }
+                }
                 if ($result === true) {
                     $company->save(false);
-                    Yii::$app->getSession()->setFlash('success', 'Настройки успешно сохранены');
+                    Yii::$app->getSession()->setFlash('success', $successMessage);
                 } else {
                     Yii::$app->getSession()->setFlash('error', 'Ошибка сохранения email для уведомления. Следующие email некорректны: ' . implode(', ', $result));
                 }
