4e5a06392d73cb575c49ef8b604ac76007d23185
Mikhail Kuklin <mk@turbodealer.ru>
2019-04-10 16:14:49 +0300

    changed config for node worker

--- js/creds_production.js
+++ js/creds_production.js
@@ -2,9 +2,9 @@ module.exports = {
     username: 'KRU7802400',
     password: 'Optima@0219',
 
-    mysqlHost: 'localhost',
-    mysqlUsername: 'alarmuser',
-    mysqlPassword: 'CW8OFDmz1yp6',
+    mysqlHost: '192.168.10.11',
+    mysqlUsername: 'robot_user',
+    mysqlPassword: 'ByYX8NPe6kbu',
     mysqlDatabase: 'alarmdb',
 
     chromeVisible: false,
