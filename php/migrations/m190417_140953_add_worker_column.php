16a4a7c8349c862eaedae54c4ce5e27fc2c94fbe
Mikhail Kuklin <mihan007@ya.ru>
2019-04-17 17:49:57 +0300

    - add support delay

---
+++ php/migrations/m190417_140953_add_worker_column.php
@@ -0,0 +1,48 @@
+<?php
+
+use app\models\Task;
+use yii\db\Migration;
+
+/**
+ * Class m190417_140953_add_worker_column
+ */
+class m190417_140953_add_worker_column extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->addColumn('task', 'worker', $this->string(12));
+        $this->createIndex('task_worker_idx', 'task', 'worker');
+        $tasks = Task::find()->all();
+        foreach ($tasks as $task) {
+            $task->save();
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        echo "m190417_140953_add_worker_column cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m190417_140953_add_worker_column cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
