4094882d81d45b945fcc6f3c821cf5f01514d1a3
Mikhail Kuklin <mihan007@ya.ru>
2019-04-17 19:58:37 +0300

    Доработки по роботу 17.04.2019г. (5 часов)
    - timeout 5 min now
    
    https://is.gd/kVsPyk

--- js/robot.js
+++ js/robot.js
@@ -1479,7 +1479,7 @@ async function robot (connection) {
     fs.mkdirSync(currentScreenshotPath)
   }
 
-  const timeoutToExecuteAllTasks = 300
+  const timeoutToExecuteAllTasks = 5*60*1000
   log(`Timeout to execute all tasks ${timeoutToExecuteAllTasks}`)
 
   cluster = await Cluster.launch({
